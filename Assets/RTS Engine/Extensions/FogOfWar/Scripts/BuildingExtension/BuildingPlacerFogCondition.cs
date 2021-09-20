70617f2ca7198b7c97f3af6f6692e2667d7e5831
Dan Marshall <daniel.marshallk@gmail.com>
2021-09-20 05:23:47 -0400

    added FoW Extension

---
+++ Assets/RTS Engine/Extensions/FogOfWar/Scripts/BuildingExtension/BuildingPlacerFogCondition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+using RTSEngine.BuildingExtension;
+using RTSEngine.Entities;
+using RTSEngine.Game;
+
+namespace RTSEngine.FoW.BuildingExtension
+{
+    public class BuildingPlacerFogCondition : MonoBehaviour, IBuildingPlacerCondition, IEntityPreInitializable
+    {
+        private IEntity entity;
+
+        [Range(0, 255), SerializeField, Tooltip("If the fog strength/intensity is smaller than this value in an area then the building can be placed in that area.")]
+        private byte minFogStrength = 120;
+
+        protected IFogOfWarRTSManager fowMgr { private set; get; } 
+
+        public void OnEntityPreInit(IGameManager gameMgr, IEntity entity)
+        {
+            this.entity = entity;
+            this.fowMgr = gameMgr.GetService<IFogOfWarRTSManager>();
+        }
+
+        public void Disable() { }
+
+        public bool CanPlaceBuilding()
+        {
+            return !fowMgr.IsInFog(entity.transform.position, minFogStrength);
+        }
+    }
+}
---
+++ Assets/RTS Engine/Extensions/FogOfWar/Scripts/FogOfWarRTSManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+using RTSEngine.Game;
+using RTSEngine.Logging;
+
+using FoW;
+
+namespace RTSEngine.FoW
+{
+    public class FogOfWarRTSManager : MonoBehaviour, IFogOfWarRTSManager 
+    {
+        [SerializeField, Tooltip("Drag and drop the FogOfWarLegacy components attached to the cameras in the map scene.")]
+        private FogOfWarLegacy[] fogOfWarCameras = new FogOfWarLegacy[0];
+
+        // FoW components:
+        private FogOfWarTeam fowHandler;
+
+        // Services
+        protected IGameLoggingService logger { private set; get; }
+
+        public void Init(IGameManager gameMgr)
+        {
+            this.logger = gameMgr.GetService<IGameLoggingService>();
+
+            fowHandler = gameObject.GetComponent<FogOfWarTeam>();
+
+            if (!logger.RequireValid(fowHandler,
+              $"[{GetType().Name}] A component of type '{typeof(FogOfWarTeam).Name}' must be added to the same game object as this component!"))
+                return; 
+
+            fowHandler.team = gameMgr.LocalFactionSlotID;
+
+            if (!logger.RequireTrue(fogOfWarCameras.Length > 0,
+              $"[{GetType().Name}] No component of type '{typeof(FogOfWarLegacy).Name}' attached to a camera has been added to the 'Fog Of War Cameras' field.",
+              type: LoggingType.warning)
+            || !logger.RequireValid(fogOfWarCameras,
+              $"[{GetType().Name}] 'Fog Of War Cameras' list field has some invalid elements."))
+                return;
+
+            foreach (FogOfWarLegacy fowCam in fogOfWarCameras)
+                fowCam.team = gameMgr.LocalFactionSlotID;
+
+            fowHandler.Reinitialize();
+        }
+
+        public bool IsInFog(Vector3 position, byte minFogStrength)
+            => fowHandler.GetFogValue(position) >= minFogStrength;
+    }
+}
